e1af1ff8c6d7aab9a6ce66803083b1f8cbaea18b
anirudh bellamkonda <anirudhit@outlook.com>
2020-01-31 15:13:45 -0600

    Create Header
    - Created header with title and navigation to github repo

---
+++ src/header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+export default function DenseAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Recipe React App
+          </Typography>
+          <div className={classes.grow} />
+          <Link href="https://github.com/anirudhit" color="inherit" target="_blank">
+            <GitHubIcon/>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
