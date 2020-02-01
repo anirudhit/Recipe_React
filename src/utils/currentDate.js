2237c5fe25a0ea7480d23848ca3de4af63553e2d
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-01 16:57:18 -0600

    Update CurrentDate
    - Removed unused variable

--- src/utils/currentDate.js
+++ src/utils/currentDate.js
@@ -1,11 +1,8 @@
 export function getCurrentDate(separator=''){
 
-    let newDate = new Date()
+    let newDate = new Date();
     let date = newDate.getDate();
-    
-    var d = new Date();
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
     let month = months[newDate.getMonth()];
     let year = newDate.getFullYear();
     
