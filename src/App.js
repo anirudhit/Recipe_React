35f02cd4749500b56142d5a63f52253736c4cd8e
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-02 00:15:43 -0600

    Update App.js
    - Added search functionality

--- src/App.js
+++ src/App.js
@@ -9,26 +9,31 @@ import RecipeLayout from './layout/Layout';
 const App =() =>{
 
   const [recipesList, setRecipesList] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("chicken");
-
-  let api_url = `${Constants.API_URL}?q=${searchQuery}&app_id=${Constants.APP_ID}&app_key=${Constants.APP_KEY}&from=0&to=10`;
+  const [searchQuery, setSearchQuery] = useState('chicken');
 
   useEffect(() =>{
     getRecipes();
-  },[]);
+  },[searchQuery]);
 
   const getRecipes = async () => {
-    const response = await fetch("./search-data.json");
+    let api_url = `${Constants.API_URL}?q=${searchQuery}&app_id=${Constants.APP_ID}&app_key=${Constants.APP_KEY}&from=0&to=10`;
+    //const response = await fetch("./search-data.json");
+    const response = await fetch(api_url);
     const data = await response.json();
     setRecipesList(data.hits);
   }
+
+  const handleSearchRecipeClick = (searchQuery) => {
+    setSearchQuery(searchQuery);
+  }
+
 
   return(
     <div className="App">
       <RecipeHeader />
 
       <div className="App-Body">
-        <QueryRecipe query={searchQuery}/>
+        <QueryRecipe onSearchRecipeClick={handleSearchRecipeClick}/>
         <RecipeLayout recipes={recipesList}/>
       </div>
 
